7503f5f4d384e3d377aa5065c5baebb54967fa26
yonghao.chen21 <tackchen@terpmail.umd.edu>
2022-03-24 12:05:39 -0400

    domain set

--- src/main/java/com/careerit/cbook/domain/AppUser.java
+++ src/main/java/com/careerit/cbook/domain/AppUser.java
@@ -1,5 +1,17 @@
 package com.careerit.cbook.domain;
 
-public class AppUser {
+import java.util.Date;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class AppUser {
+	private Long uid;
+	private String name;
+	private String password;
+	private String email;
+	private Date dob;
+	private String mobile;
 }
--- src/main/java/com/careerit/cbook/domain/Contact.java
+++ src/main/java/com/careerit/cbook/domain/Contact.java
@@ -1,5 +1,17 @@
 package com.careerit.cbook.domain;
 
-public class Contact {
+import java.util.Date;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Contact {
+	private Long cid;
+	private String name;
+	private int status;
+	private String email;
+	private Date dob;
+	private String mobile;
 }
